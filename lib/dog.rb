df9785c9a231b19d56994c600bfee177d6444dd4
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-08 13:59:58 +0300

    completed tests

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
+    def name=(name)
+        @name=name
+    end
 
+    def name
+        @name
+    end
+
+    def breed= (breed)
+        @breed= breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+rufus = Dog.new
+rufus.name = "Rufus"
+
+corgi = Dog.new
+corgi.breed = "Corgi"
+
+spicy = Dog.new
+spicy.name = "Spicy"
+
+chihuahua = Dog.new
+chihuahua.breed = "Chihuahua"
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,29 @@
 class Person
 
+    #setter method
+    def name=(name)
+        @name = name
+    end
+
+    #getter method
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+    def job
+        @job
+    end
+
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+kanye.name
+
+mechanic = Person.new
+mechanic.job = "Mechanic"
+puts mechanic.job
